49b29380144ce23e448d1ba28dbd210467bd5ea9
Zhiji Gu <zhiji_gu@feiliks.com>
2019-09-20 12:24:22 +0800

    add eureka client

--- src/main/java/com/feiliks/sap_bridge/BootApplication.java
+++ src/main/java/com/feiliks/sap_bridge/BootApplication.java
@@ -3,11 +3,13 @@ package com.feiliks.sap_bridge;
 import com.feiliks.sap_bridge.utils.JCoUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.io.IOException;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BootApplication {
 
     public static void main(String[] args) throws IOException {
